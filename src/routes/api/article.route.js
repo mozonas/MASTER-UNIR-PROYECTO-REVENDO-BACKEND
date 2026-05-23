215d134f61b0976fc940c11e75782c9662481646
ky9xtd7nvy-ship-it <ky9xtd7nvy@privaterelay.appleid.com>
2026-05-24 00:24:42 +0200

    Crear archivos entidad categoria

---
+++ src/routes/api/article.route.js
@@ -0,0 +1,7 @@
+/* 1. Importar el modulo Router de express
+2. Importar las funciones del controlador de articulos
+3. Importar la funcion intermedia de validacion de articulos
+4. Crear una instancia del Router
+5. Definir las rutas para cada operacion (GET, POST, PUT, DELETE)
+6. Exportar el router para ser utilizado en el servidor
+*/
