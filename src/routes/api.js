dd853c3b50cc6d5e0aa9824504e3ce91ed3f3f31
David Rodríguez Marbán <davidrodriguezmarban@gmail.com>
2026-06-03 14:44:20 +0200

    Fix: api routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,12 +5,11 @@ const { checkToken } = require('../middlewares/auth.middleware');
 
 // Rutas de /api
 router.use('/users', require('./api/users'));
-// router.use('/signup', require('./api/signup'));
+router.use('/signup', require('./api/signup'));
 router.use('/login', require('./api/login'));
 
 //router.use('/messages', require('./api/messages'));
 //router.use('/transactions', require('./api/transactions'))
-// 30052026 - F4eature Login/SignUp
 
 
 
