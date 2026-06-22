655e6cea03b8c7b396913c04a23cd61647626475
carlesgenero-arch <carlesgenero@gmail.com>
2026-06-22 11:14:32 +0200

    fix: mejora funcionalidades controller y modelos para tablas actividad

--- src/routes/api/article.js
+++ src/routes/api/article.js
@@ -25,10 +25,8 @@ console.log("Router de articulos creado.");
 // 👉 SIEMPRE PRIMERO
 router.get("/enums", getEnums);
 router.get("/search", searchArticles);
-// Ruta para extraer articulos publicados este mes y el anterior
-router.get ('/published/this-month', articleController.getThisMonth);
-router.get ('/published/last-month', articleController.getLastMonth);
-
+// Ruta para extraer comparación de articulos publicados este mes con el anterior
+router.get ('/published/comparison', articleController.getPublishedComp);
 // Ruta para extraer articulos vendidos este mes
 router.get ('/sold/:month', articleController.getSoldThisMonth);
 // Ruta para extraer articulos vendidos este año
